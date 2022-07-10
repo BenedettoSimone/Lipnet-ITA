5ef822fab65b45215d60fe7a0ffa3d9a151c4b97
Benedetto Simone <sim.benede@gmail.com>
2022-07-10 23:29:28 +0200

    clean repo

--- change_fps/change_fps.py
+++ change_fps/change_fps.py
@@ -1,30 +1,27 @@
 import os
-import ffmpeg
-
-
 def create_directory(directory):
     try:
         os.makedirs(directory, exist_ok=True)
     except OSError as error:
         print("Directory can not be created")
 
+
+
+
 if __name__ == '__main__':
 
-    os.chdir('DATASET')
+    os.chdir('../DATASET')
 
     for s in os.listdir():
         if '.DS_Store' not in s:
             os.chdir(s)
 
-            create_directory('../../25fps/'+s)
+            create_directory('../../25fps/' + s)
 
             for video in os.listdir():
-                stream = ffmpeg.input(video)
-                video_stream = stream.video
-                audio_stream = stream.audio
-                stream = stream.filter('fps', fps=25, round='up')
-                stream = ffmpeg.output(audio_stream, video_stream, '../../25fps/'+s+'/'+video)
-                ffmpeg.run(stream)
+
+                os.system("ffmpeg -i " +video+" -filter:v fps=fps=25 "+'../../25fps/' + s + '/' + video)
+
             os.chdir('..')
     os.chdir('..')
 
