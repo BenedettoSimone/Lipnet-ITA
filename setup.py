4cfc201c0fb0a8efb23abeead554407911161809
benedettosimone <sim.benede@gmail.com>
2022-11-17 11:26:49 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lipnet',
+    version='0.1',
+    packages=['lipnet'],
+    url='https://github.com/BenedettoSimone/researchProject',
+    license='',
+    author='benedettosimone',
+    author_email='sim.benede@gmail.com',
+    description='LipReadingITA: Keras implementation of the method described in the paper \'LipNet: End-to-End Sentence-level Lipreading\''
+)
